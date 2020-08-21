3dc71681514de3c84e8e61a96156004b4ecc7901
Hector Martinez-Seara <hseara@gmail.com>
2020-08-21 16:15:27 +0200

    Added A1 courses for np and mpl

---
+++ slides.sh
@@ -0,0 +1,33 @@
+# Python-C2 course (PyAdv-C2)
+#############################
+
+##########################################
+#  Scientific Python-numpy course (SPy)  #
+##########################################
+
+# Scientific Python - numpy_scipy (SPy-np)
+############################################
+jupyter nbconvert SPy-np-A1-00.ipynb --to slides
+
+
+
+# Scientific Python - matplotlib (SPy-mpl)
+############################################
+jupyter nbconvert SPy-mpl-A1-00.ipynb --to slides
+jupyter nbconvert SPy-mpl-A1-01.ipynb --to slides
+
+
+
+# Scientific Python - pandas (SPy-pd)
+############################################
+jupyter nbconvert SPy-pd-A1-00.ipynb --to slides
+
+
+# Machine learning Python (MLPy-0)
+####################################
+
+
+
+# Imaging Python (ImgPy-0)
+####################################
+
