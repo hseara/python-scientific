1a7a73a49b685aba15048a9bd51b7fff4a5d63ec
Hector Mtz-Seara <hseara@gmail.com>
2021-07-12 20:43:20 +0200

    Updated course including pandas

--- slides.sh
+++ slides.sh
@@ -1,18 +1,17 @@
-# Python-C2 course (PyAdv-C2)
-#############################
-
 ##########################################
 #  Scientific Python-numpy course (SPy)  #
 ##########################################
 
 # Scientific Python - numpy_scipy (SPy-np)
 ############################################
-jupyter nbconvert SPy-np-a1-00.ipynb --to slides
+rm -f numpy/*html
+jupyter nbconvert numpy/SPy-np-a1-00.ipynb --to slides
 
 
 
 # Scientific Python - matplotlib (SPy-mpl)
 ############################################
+rm -f matplotlib/*html
 jupyter nbconvert SPy-mpl-a1-00.ipynb --to slides
 jupyter nbconvert SPy-mpl-a1-01.ipynb --to slides
 
@@ -20,14 +19,23 @@ jupyter nbconvert SPy-mpl-a1-01.ipynb --to slides
 
 # Scientific Python - pandas (SPy-pd)
 ############################################
-jupyter nbconvert SPy-pd-A1-00.ipynb --to slides
-
-
-# Machine learning Python (MLPy-0)
+rm -f pandas/*html
+jupyter nbconvert SPy-pd-a1-01.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-02.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-03.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-04.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-05.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-06.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-07.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-08.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-09.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-10.ipynb --to slides
+jupyter nbconvert SPy-pd-a1-11.ipynb --to slides
+# Machine learning Python (SPy-ml)
 ####################################
 
 
 
-# Imaging Python (ImgPy-0)
+# Imaging Python (SPy-img)
 ####################################
 
